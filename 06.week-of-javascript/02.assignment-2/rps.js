d41c497f7fd40e7e55b007901a994ac8deba64f2
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-05-15 15:59:01 +0300

    complete the rps assignment

--- 06.week-of-javascript/02.assignment-2/rps.js
+++ 06.week-of-javascript/02.assignment-2/rps.js
@@ -2,7 +2,6 @@
 // Objective: Create a two-player Rock Paper Scissors game in JavaScript,
 // with one player being the user and the other being the computer.
  
- 
 // Functionality: 
 // User selects their choice (rock, paper, or scissors) through buttons or 
 // user input.
@@ -24,6 +23,77 @@
 // Utilize default parameters in a function
 
 
+const +user_move = prompt('Interested in a rock,paper,scissors game?(Yes/no)' ) ? 
+				prompt('which will you choose? 1-> rock, 2->paper,3->scissors') :
+				prompt('okay.maybe next time')
+
+let possible_choices = ['rock', 'paper','scissors']
+let compWins
+let userWins 
+
+function getComputerMove(){
+	let random_number = Math.random()
+	if (random_number <= 0.33){
+		let actualCompMove = possible_choices[0]
+		return actualCompMove
+	} else if (random_number <=0.66){
+		let actualCompMove = possible_choices[1]
+		return actualCompMove
+	} else if (random_number >= 0.67){
+		let actualCompMove = possible_choices[2]
+		return actualCompMove
+	} else {
+		return 'An error occurred'
+	}
+}
+
+function getUserMove(user_move){
+	if (user_move === 1){
+		let actualUserMove = possible_choices[0]
+		return actualUserMove
+	} else if (user_move === 1){
+		let actualUserMove = possible_choices[1]
+		return actualUserMove
+	} else if (user_move === 1){
+		let actualUserMove = possible_choices[2]
+		return actualUserMove
+	} else {
+		return 'An error occurred'
+	}
+}
+
+function determineWinner(){
+	if (actualUserMove === actualCompMove){
+		let winner = 'Its a tie'
+		return winner  
+	} else if(actualUserMove === possible_choices[0] && actualCompMove === possible_choices[2]){
+		let winner = 'the user wins'
+		userWins++
+		return winner
+	} else if(actualUserMove === possible_choices[1] && actualCompMove === possible_choices[0]){
+		let winner = 'the user wins'
+		userWins++
+		return winner
+	} else if(actualUserMove === possible_choices[2] && actualCompMove === possible_choices[1]){
+		let winner = 'the user wins'
+		userWins++
+		return winner
+	} else if(actualUserMove === possible_choices[1] && actualCompMove === possible_choices[2]){
+		let winner = 'the computer wins'
+		compWins++
+		return winner
+	} else if(actualUserMove === possible_choices[0] && actualCompMove === possible_choices[1]){
+		let winner = 'the computer wins'
+		compWins++
+		return winner
+	} else if(actualUserMove === possible_choices[2] && actualCompMove === possible_choices[0]){
+		let winner = 'the computer wins'
+		compWins++
+		return winner
+	}
+
+} 
+
 
 // Cheat Mode:
  
@@ -31,3 +101,10 @@
 // way (optional).
 // Come up with one specific implementation of cheat mode (e.g., computer
 // always chooses scissors).
+
+for(let i=game_number,i<total_number_games,i++){
+	if(i % 2 === 1){
+		actualCompMove = possible_choices[2]
+		return actualCompMove
+	}
+}
