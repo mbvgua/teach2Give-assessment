ef81d29497cda8e7518bb0cfdc9a39660faa5c2e
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-05-15 14:58:35 +0300

    create initial project files

---
+++ 06.week-of-javascript/02.assignment-2/rps.js
@@ -0,0 +1,33 @@
+// Requirements:
+// Objective: Create a two-player Rock Paper Scissors game in JavaScript,
+// with one player being the user and the other being the computer.
+ 
+ 
+// Functionality: 
+// User selects their choice (rock, paper, or scissors) through buttons or 
+// user input.
+// Computer randomly chooses its move (0-0.33 rock, 0.34-0.66 paper,
+//  0.67-1 scissors).
+// Winner is determined based on standard rock paper scissors rules.
+// Display the result (win, lose, tie) and user/computer choices on 
+// the screen.
+// Consider adding scorekeeping and a winner declaration for multiple 
+// rounds (optional).
+
+// Code Structure:
+ 
+// Use at least one function declaration.
+// Use at least one function expression.
+// Use at least one arrow function.
+// Use at least one anonymous function.
+// Use a ternary operator for a concise outcome display 
+// Utilize default parameters in a function
+
+
+
+// Cheat Mode:
+ 
+// Design a "cheat mode" feature that alters the computer's choice in some
+// way (optional).
+// Come up with one specific implementation of cheat mode (e.g., computer
+// always chooses scissors).
