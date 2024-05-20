35ca6a15b204bfd649ac1d64ff30d405f774a892
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-05-20 18:52:12 +0300

    05.question-1

--- 06.week-of-javascript/05.assignment-4-advanved-functions/1.recursive-function.js
+++ 06.week-of-javascript/05.assignment-4-advanved-functions/1.recursive-function.js
@@ -1,4 +1,35 @@
 // Write a recursive function in JavaScript that calculates the 
 // factorial of a given non-negative integer n.
 
+function isNonNegative(n){
+    // function to check if numbe ris a non-negative val
+    // i.e 0 or > 0 (+ve numbers)
+
+    const numVal = (n >= 0 && Number.isInteger(n)) ? true : false
+    return numVal
+}
+
+function returnFactorial(n){
+    //function to return the factorial of a number
+    let errorMsg = 'A non-negative number'
+
+    if (!isNonNegative(n)){
+        return errorMsg
+    } else if (n===1){
+        return n
+    } else {
+        // if no is not 1 or non-negative execute this
+        answer = n * returnFactorial(n-1)
+        return answer
+    }
+}
+
+console.log(returnFactorial(9))
+console.log(returnFactorial(4))
+console.log(returnFactorial(-9))
+console.log(returnFactorial(129))
+console.log(returnFactorial(5.9))
+console.log(returnFactorial(59))
+console.log(returnFactorial(17))
+console.log(returnFactorial(-7))
 
