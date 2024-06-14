23fcc5e4bab9211c3eb6e5ac73ad44a631019ca0
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-06-14 14:44:40 +0300

    get the server running

---
+++ 09.week-of-node/6.friday-assesment/src/server.ts
@@ -0,0 +1,21 @@
+import express, {json} from 'express'
+// import productRouter from './routes/productRoutes'
+// import categoriesRouter from './routes/categoryRoutes'
+// import authRouter from './routes/authRoutes'
+
+
+const app = express()   // initialize the application
+
+app.use(json())          //add a body to the requests
+
+
+// // add all the middlewares and urls
+// app.use("/products",productRouter)
+// app.use("/categories", categoriesRouter)
+// app.use("/auth", authRouter)
+
+
+// start the application
+app.listen(4000,()=>{
+    console.log('Wagwan. Server inarun...')
+})
