2426ff3cdf7acf939d6fe50289c2caecd12f5517
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-06-14 17:10:29 +0300

    allow for search product functionality

--- 09.week-of-node/6.friday-assesment/src/routes/productRoutes.ts
+++ 09.week-of-node/6.friday-assesment/src/routes/productRoutes.ts
@@ -8,8 +8,8 @@ productRouter.post("", addProduct)
 productRouter.get("/:id", getProduct)
 productRouter.get("", getProducts)
 productRouter.get("/search ", searchProduct)
+productRouter.get("/filter", paginateProducts) 
 productRouter.delete("/:id", deleteProduct)
-productRouter.get("/filter", paginateProducts) //-> 2 post requests?!?!
 
 
 export default productRouter
