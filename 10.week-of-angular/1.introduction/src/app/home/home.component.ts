5d93d14016fba2caeee2501819889d3d74e9065f
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-06-19 06:43:39 +0300

    data binding in the home component

--- 10.week-of-angular/1.introduction/src/app/home/home.component.ts
+++ 10.week-of-angular/1.introduction/src/app/home/home.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  //string interpolation
   greetings:String = 'Niaje Morio!'
+  isLying = true
+  people = [1,'Basmati','Rice','Nuclear',['innner','nesting',['continues']]]
+
+  sayHello():string{  //string interpolated as it returns a string
+    return 'Hello there You!'
+  }
+
+  // property binding
+  disable=true
+  // experimental
+
+  constructor(){
+    setTimeout(()=>{
+      this.disable = false
+      // this.experimental.style.color = 'red'
+    },3000)
+  }
+
+  // event binding
+  AddThem(){
+    console.log('you have clicked the button')
+  }
+
+  takeInput(event:Event){
+    console.log(event.target)
+  }
+
+  // 2 way data binding
+  workWithThis="palceholder text"
 }
