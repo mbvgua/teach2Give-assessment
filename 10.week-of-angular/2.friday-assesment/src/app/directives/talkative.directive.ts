90cf79d45560c60c4465b49744b796a1b42f6c2d
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-06-21 15:34:44 +0300

    generate the directives

---
+++ 10.week-of-angular/2.friday-assesment/src/app/directives/talkative.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { MessageService } from '../services/messages/messages.service';
+
+@Directive({
+  selector: '[appTalkative]',
+  standalone: true
+})
+export class TalkativeDirective {
+
+  constructor(private el: ElementRef, private messageService: MessageService) {}
+
+  @HostListener('click') onClick() {
+    const message = this.messageService.getRandomMessage();
+    this.el.nativeElement.innerText = message;
+  }
+}
