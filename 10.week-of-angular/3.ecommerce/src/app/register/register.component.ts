8c548b83adfa914e32cdd8dc3f3f9ffd21a35ea0
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-06-25 07:04:56 +0300

    basic reactive forms(register component)

--- 10.week-of-angular/3.ecommerce/src/app/register/register.component.ts
+++ 10.week-of-angular/3.ecommerce/src/app/register/register.component.ts
@@ -1,14 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AsyncValidatorFn, FormArray, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { AsyncValidatorFn, FormArray, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  form!: FormGroup
+  
+  onSubmit(){
+    console.log(this.form.value)
+    this.form.reset()
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl(null,Validators.required),
+      email: new FormControl(null,[Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required),
+      confirm_password: new FormControl(null, Validators.required)
+    })
+  }
 
 }
